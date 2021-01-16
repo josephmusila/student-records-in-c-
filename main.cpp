1bac60d30916f831c26515b204f50466d568809b
josephmusila <josemusila03@gmail.com>
2021-01-15 23:08:13 -0800

    added file storage

--- main.cpp
+++ main.cpp
@@ -21,6 +21,8 @@ public:
     void show_data();
     void  add_data();
     void  write_rec();
+    void  update_rec();
+    void select_rec();
 
 
 };
@@ -32,6 +34,7 @@ void add_records::add_data()
     cin>>name;
     cout<<"enter your index"<<endl;
     cin>>index;
+
     cout<<"enter your gpa;"<<endl;
     cin>>gpa;
 }
@@ -60,12 +63,45 @@ void add_records::read_data(){
         return;
     }
     cout<<"\n********Data from file*********"<<endl;
-    while(infile.eof()){
+    while(!infile.eof()){
         if(infile.read(reinterpret_cast<char *>(this),sizeof(*this))>0){
            show_data();
            }
     }
     infile.close();
+
+}
+void add_records::select_rec()
+{
+     cout<<"enter your name"<<endl;
+    cin>>name;
+}
+void add_records::update_rec()
+{
+
+    int n;
+    fstream iofile;
+    iofile.open("record.txt",ios::in|ios::binary);
+    if(!iofile)
+    {
+        cout<<"error openning the file! File not found"<<endl;
+        return;
+    }
+    iofile.seekg(0, ios::end);
+    int count=iofile.tellg()/sizeof(*this);
+    cout<<"There are "<<count<< " records in the file "<<endl;
+    cout<<"Enter the record to edit"<<endl;
+    cin>>n;
+    iofile.seekg((n-1)*sizeof(*this));
+    iofile.read(reinterpret_cast<char*>(this),sizeof(*this));
+    cout<<"Record "<<n<<" has the following data "<<endl;
+    show_data();
+    iofile.close();
+    iofile.open("records.txt",ios::out|ios::in|ios::binary);
+    iofile.seekp((n-1)*sizeof(*this));
+    cout<<"Enter the name to modify: "<<endl;
+    add_data();
+    iofile.write(reinterpret_cast<char*>(this),sizeof(*this));
 
 }
 int main()
@@ -77,19 +113,23 @@ int main()
         cout<<"Select an option below"<<endl;
         cout<<"\n\t1-->Add record to file"<<endl;
         cout<<"\n\t2-->Show record from file"<<endl;
-        cout<<"\n\t3-->quit"<<endl;
+        cout<<"\n\t3-->Update record "<<endl;
+        cout<<"\n\t4-->quit"<<endl;
         cout<<"Enter your Choice:"<<endl;
         cin>>choice;
-
+//HePLWV2u
         switch(choice)
         {
         case 1:
             A.add_data();
             break;
         case 2:
-            A.show_data();
+            A.read_data();
             break;
         case 3:
+            A.update_rec();
+            break;
+        case 4:
             exit(0);
             break;
         default:
